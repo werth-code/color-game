e32e20b3517bcb133507c803bb451ebc98197267
Matt Werth <matthewwerth@gmail.com>
2020-07-31 12:54:32 -0400

    Working Version1 Of Color Game

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,8 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+let colors = generateRandomColors(6) //
 
 const message = document.getElementById('message')
 const squares = document.querySelectorAll('.square')
-const pickedColor = colors[3]
+const pickedColor = randomColor()
 const colorDisplay = document.getElementById('colorDisplay')
 
 colorDisplay.textContent = pickedColor
@@ -38,4 +31,22 @@ function changeColors(color) {
         let current = squares[i]
         current.style.backgroundColor = color
     }
+}
+
+function randomColor() {
+    let number = Math.floor(Math.random() * colors.length)
+    return colors[number]
+}
+
+function rgbColorGen() {
+   return Math.floor(Math.random() * 255)
+}
+
+function generateRandomColors(num) {
+    let randomArr = []
+    for(let i = 0; i < num; i++) {
+        let newColor = `rgb(${rgbColorGen()}, ${rgbColorGen()}, ${rgbColorGen()})`
+        randomArr.push(newColor)
+    }
+    return randomArr
 }
